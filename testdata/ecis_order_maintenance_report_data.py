35c02a68df869c6496c71c929daeafc256867317
Valasa Sreeja (External - Capgemini Sverige AB) <valasa.sreeja@ikea.com>
2026-05-19 03:34:40 -0700

    Updated Order_maintenance_report_page

--- testdata/ecis_order_maintenance_report_data.py
+++ testdata/ecis_order_maintenance_report_data.py
@@ -1,3 +1,58 @@
+TEST_REPORT=[{
+    "supplier":"50029"
+},
+{
+    "supplier":"23231",
+}]
+TEST_ORDER_REPORT_SEARCH = [
+    {
+        "database": "Production",
+        "scode": "50029",
+        "rcv": "1406 - LSC",
+        "order_type": "L",
+        "art_no": "00517060"
+    },
+    {
+        "database": "Production",
+        "scode": "50029",
+        "rcv": "1441 - LSC",
+        "order_type": "L",
+        "art_no": "00517098"
+    },
+    {
+        "database": "Production",
+        "scode": "50029",
+        "rcv": "1573 - LSC",
+        "order_type": "L",
+        "art_no": "00517060"
+    }
+]
+TEST_COPY_CLIP =[
+    {
+        "database": "Production",
+        "scode": "15653",
+    }
+]
+
+TEST_PRINT_AND_DOWNLOAD =[
+    {
+        "database": "Production",
+        "scode": "15653",
+    }
+]
+
+TEST_REPORT_EXPORT = [ {
+    "database": "Production",
+     "scode": "15653",
+     "export_option":"PDF"
+},
+    {
+        "database": "Production",
+        "scode": "15653",
+    "export_option":"Microsoft Excel(XLS)"
+    }
+]
+
 TEST_ORDER_REPORTS_MATRIX =[
     {
          "scode": "23231",
@@ -13,3 +68,4 @@ TEST_SAVE_ORDER_REPORT =[
             ],
         "query_name":"Order Report"
     }]
+
